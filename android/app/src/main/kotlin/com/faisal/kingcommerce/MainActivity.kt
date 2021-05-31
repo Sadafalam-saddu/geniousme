17dc105b7d854cd96df6edf8320cc489a53f6b18
Faisa Kabir <faisalkabir95@gmail.com>
2021-05-31 22:26:05 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/faisal/kingcommerce/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.faisal.geniouscart
+
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.util.Base64
+import android.util.Log
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        printHashKey(this)
+    }
+
+    fun printHashKey(pContext: Context) {
+        var TAG : String = "Hash Key"
+        try {
+            val info: PackageInfo = pContext.getPackageManager().getPackageInfo(pContext.getPackageName(), PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey = String(Base64.encode(md.digest(), 0))
+                Log.i(TAG, "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e(TAG, "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e(TAG, "printHashKey()", e)
+        }
+    }
+}
